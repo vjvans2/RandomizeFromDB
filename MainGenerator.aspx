f1fa09aeaf7deca0ffb26bfcf8ceaad13cdb8f32
Vinny VanSlyke <vincent.vanslyke@gmail.com>
2017-09-11 12:57:08 -0400

    Add files via upload

---
+++ MainGenerator.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MainGenerator.aspx.cs" Inherits="PokemonRandomName.MainGenerator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Button ID="btnRandom" runat="server" Text="Random Pkmn Name" OnClick="btnRandom_Click" />
+        <br /><br />
+        <asp:Label ID="lblPkmn" runat="server" Text=""></asp:Label>
+        <br /><br />
+        <asp:DropDownList ID="ddlPokemon" AutoPostBack="true" runat="server" DataSourceID="SqlDataSource1" 
+            DataTextField="PokemonName" DataValueField="PokemonID"> </asp:DropDownList>
+
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:PKMNConnectionString %>" 
+            SelectCommand="SELECT [PokemonID], [PokemonName] FROM [Pokemon2] ORDER BY [PokemonID]">
+        </asp:SqlDataSource>
+    </form>
+</body>
+</html>
---
+++ MainGenerator.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace PokemonRandomName
+{
+    public partial class MainGenerator : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnRandom_Click(object sender, EventArgs e)
+        {
+            //Functioning Code 1.0
+            Random r = new Random();
+            int countDDLEntries = ddlPokemon.Items.Count;
+            int randomPick = r.Next(0, (countDDLEntries - 1));
+            ddlPokemon.SelectedIndex = randomPick;  //specify what I'm talking to, specify selected index that we're using
+            //string pkmn = ddlPokemon.Text[randomPick].ToString();
+            string pkmn = ddlPokemon.SelectedItem.ToString();
+            lblPkmn.Text = pkmn;
+        }
+
+        //protected void NextLevelRandom2()
+        //{
+        //    Random r = new Random();
+        //    List<int> storedInt = new List<int>(ddlPokemon.Items.Count);
+
+
+        //    int countDDLEntries = ddlPokemon.Items.Count;
+        //    int randomPick = r.Next(0, (countDDLEntries - 1));
+
+        //    ddlPokemon.SelectedIndex = randomPick;
+        //    string pkmn = ddlPokemon.SelectedItem.ToString();
+        //    lblPkmn.Text = pkmn;
+        //}
+    }
+}
