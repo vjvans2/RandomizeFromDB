0c09056467f6fb45ece222b9a4934d60e3d23617
Vinny VanSlyke <vincent.vanslyke@gmail.com>
2017-09-14 19:26:58 -0400

    Update MainGenerator.aspx.cs

--- MainGenerator.aspx.cs
+++ MainGenerator.aspx.cs
@@ -19,49 +19,13 @@ namespace PokemonRandomName
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            //if (!IsPostBack)
-            //{
-            //    CallDBOnce();
-            //}            
+          
         }
 
         protected void btnRandom_Click(object sender, EventArgs e)
         {
             GrabPokemonInfo2();            
         }
-
-        public void GrabPokemonInfo()
-        {
-            //List<Pokemon> retrievedPokemon = PkmnDB.GetPokemonFromDB();
-            ////List<int> pkmnIdNumbers = new List<int>(); //stuck in the class scope
-            ////List<string> pkmnNames = new List<string>();
-            //foreach (Pokemon p in retrievedPokemon)  //each button click is refilling the list
-            //{
-            //    pkmnIdNumbers.Add(p.PokemonID);
-            //    pkmnNames.Add(p.PokemonName);
-            //}
-            //Random r = new Random();
-            //int startNumID = pkmnIdNumbers[0]; //pkmn from 1-9, indexes from 0-8
-            //int countNumID = pkmnIdNumbers.Count;
-            //int randomPick = r.Next((startNumID-1), (countNumID)); //indexes need to go from [first value] to [final value +1] but we need the -1 to account for index 0
-           
-            //if (pkmnIdNumbers != null)
-            //{
-            //    pkmnIdNumbers.RemoveAt(randomPick);
-            //}
-            //else
-            //{
-            //    foreach (Pokemon p in retrievedPokemon)
-            //    {
-            //        pkmnIdNumbers.Add(p.PokemonID);
-            //    }
-            //}
-
-            ////List<int> pickedNumbers = new List<int>();
-            //pickedNumbers.Add(randomPick);   
-            //string chosenPokemon = pkmnNames[randomPick];
-            //lblPkmn.Text = chosenPokemon;  
-        } //isolated is fine
 
         public void GrabPokemonInfo2()
         {
